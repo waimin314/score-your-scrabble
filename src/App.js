7e6dcb46c35876a386c37860a4d41bccfded0f4a
Wai Min <leopard23@gmail.com>
2020-09-14 22:56:55 +0800

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -13,19 +13,31 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>{letters}</h1>
+    <div className='flex flex-col items-center '>
+      <h1 className='h-10 mt-10 text-2xl'>{letters}</h1>
       <form>
         <input
+          className='my-3 p-2'
           type='text'
           value={letters}
           autoFocus
           onChange={(e) => handleChange(e)}
         ></input>
-        <button onClick={() => setLetters('')}>Clear</button>
-        <button>Save</button>
-        <button>View All</button>
       </form>
+      <div className='flex space-x-5'>
+        <button
+          className='w-20 h-8 rounded-md bg-pink-700 text-white'
+          onClick={() => setLetters('')}
+        >
+          Clear
+        </button>
+        <button className='w-20 h-8 rounded-md bg-indigo-700 text-white'>
+          Save
+        </button>
+        <button className='w-20 h-8 rounded-md bg-indigo-700 text-white'>
+          View All
+        </button>
+      </div>
     </div>
   );
 }
