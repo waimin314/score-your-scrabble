ffbd762c81b8b3af8a15d2fd71773b0c310a5456
Wai Min <leopard23@gmail.com>
2020-09-15 23:12:13 +0800

    Implement simple express server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('Hola Mola');
+const express = require('express');
+
+const app = express();
+
+app.get('/entries', (req, res) => {
+  res.send('Hola Mola');
+});
+
+app.post('/entries', (req, res) => {
+  res.sendStatus(200);
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`listening at ${PORT}`);
+});
