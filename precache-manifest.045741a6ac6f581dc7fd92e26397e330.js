bfdae44fb545e6d51e858e57684d2992e6ec8a0a
Wai Min <leopard23@gmail.com>
2020-09-15 09:26:16 +0800

    Updates

---
+++ precache-manifest.045741a6ac6f581dc7fd92e26397e330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145c624ab03a52db180b340b67ca0e2f",
+    "url": "/score-your-scrabble/index.html"
+  },
+  {
+    "revision": "b7e17cfb13e55eed85ba",
+    "url": "/score-your-scrabble/static/css/main.fe9c2f52.chunk.css"
+  },
+  {
+    "revision": "f9333e40dc1864acfd65",
+    "url": "/score-your-scrabble/static/js/2.a062469b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/score-your-scrabble/static/js/2.a062469b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e17cfb13e55eed85ba",
+    "url": "/score-your-scrabble/static/js/main.86f6fda5.chunk.js"
+  },
+  {
+    "revision": "4bb1d22845f8a6b9872f",
+    "url": "/score-your-scrabble/static/js/runtime-main.400965b6.js"
+  }
+]);
