cc5391b2bcbaa6b2360ec5a29f75f27d6e29036d
Wai Min <leopard23@gmail.com>
2020-09-20 10:58:00 +0800

    Crate ViewAll component for entries

---
+++ client/src/components/ViewAll.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function ViewAll({ entries }) {
+  const renderTable = () => {
+    return entries.map(({ word, score }, index) => {
+      const color = index % 2 === 0 ? 'bg-white' : 'bg-gray-300';
+      return (
+        <tr className={`${color}`}>
+          <td className='border border-gray-700 px-4 py-2'>{word}</td>
+          <td className='border border-gray-700 px-4 py-2'>{score}</td>
+        </tr>
+      );
+    });
+  };
+  return (
+    <table className='table-fixed'>
+      <thead>
+        <tr className='bg-gradient-to-r from-teal-400 to-blue-300 text-gray-800'>
+          <th className='w-1/2 border border-gray-700 px-4 py-2'>Word</th>
+          <th className='w-1/3 border border-gray-700 px-4 py-2'>Score</th>
+        </tr>
+      </thead>
+      <tbody className='odd:bg-gray-200'>{renderTable()}</tbody>
+    </table>
+  );
+}
