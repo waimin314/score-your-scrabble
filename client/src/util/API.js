fd9a3aa6a3ddc9a49ce89936eb1bc35adc76f4c0
Wai Min <leopard23@gmail.com>
2020-09-20 20:28:36 +0800

    Update client to retrieve server url from env

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/';
+const BASE_URL = process.env.SERVER_URL || 'http://localhost:5000/';
 const API = axios.create({ baseURL: BASE_URL });
 
 const getAllEntries = async () => {
