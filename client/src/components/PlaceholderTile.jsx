c96a2ee8d4ee91c9bb95e0b8c6dcfef86dc7a780
Wai Min <leopard23@gmail.com>
2020-09-20 10:20:22 +0800

    Imporove shadows on Tiles

--- client/src/components/PlaceholderTile.jsx
+++ client/src/components/PlaceholderTile.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function PlaceholderTile() {
   return (
-    <div className='flex flex-col w-12 h-16  border-gray-800 border-dashed bg-gray-500 m-3 rounded-md shadow-2xl border-2 md:w-20 md:h-24 md:border-4'></div>
+    <div className='flex flex-col w-12 h-16  border-gray-800 border-dashed bg-gray-500 m-3 rounded-md shadow-md border-2 md:w-20 md:h-24 md:border-4 md:shadow-lg'></div>
   );
 }
--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Tile({ letter, point }) {
   return (
-    <div className='flex flex-col w-12 h-16 m-3 items-center rounded-md shadow-2xl border-t-2 border-r-4 border-b-4 border-yellow-500 bg-gradient-to-br from-yellow-400 to-yellow-300 md:w-20 md:h-24 md:rounded-md md:border-t-4 md:border-b-8 md:border-r-8'>
+    <div className='flex flex-col w-12 h-16 m-3 items-center rounded-md shadow-md border-t-2 border-r-4 border-b-4 border-yellow-500 bg-gradient-to-br from-yellow-400 to-yellow-300 md:w-20 md:h-24 md:rounded-md md:border-t-4 md:border-b-8 md:border-r-8 md:shadow-md'>
       <h1 className='m-auto text-3xl font-serif font-semibold md:text-5xl'>
         {letter}
         <sub className='text-sm font-sans md:text-xl'>{point}</sub>
