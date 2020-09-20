d22a6ec95141e6ba26af5caa3f923741bb3ee2a4
Wai Min <leopard23@gmail.com>
2020-09-20 14:52:11 +0800

    Add routing ViewAllPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import ViewAllPage from './pages/ViewAllPage';
 
 function App() {
   return (
-    <div>
-      <MainPage />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/view-all'>View All</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path='/view-all'>
+            <ViewAllPage />
+          </Route>
+          <Route path='/'>
+            <MainPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/pages/ViewAllPage.jsx
+++ client/src/pages/ViewAllPage.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllEntries } from './../util/API';
 
-export default function ViewAll({ entries }) {
+export default function ViewAll() {
+  const [entries, setEntries] = useState([]);
+  useEffect(async () => {
+    setEntries(await getAllEntries());
+  }, []);
   const renderTable = () => {
     return entries.map(({ word, score }, index) => {
       const color = index % 2 === 0 ? 'bg-white' : 'bg-gray-300';
@@ -13,14 +18,16 @@ export default function ViewAll({ entries }) {
     });
   };
   return (
-    <table className='table-fixed'>
-      <thead>
-        <tr className='bg-gradient-to-r from-teal-400 to-blue-300 text-gray-800'>
-          <th className='w-1/2 border border-gray-700 px-4 py-2'>Word</th>
-          <th className='w-1/3 border border-gray-700 px-4 py-2'>Score</th>
-        </tr>
-      </thead>
-      <tbody className='odd:bg-gray-200'>{renderTable()}</tbody>
-    </table>
+    <div className='flex justify-center mt-5'>
+      <table className='table-fixed'>
+        <thead>
+          <tr className='bg-gradient-to-r from-teal-400 to-blue-300 text-gray-800'>
+            <th className='w-1/2 border border-gray-700 px-4 py-2'>Word</th>
+            <th className='w-1/3 border border-gray-700 px-4 py-2'>Score</th>
+          </tr>
+        </thead>
+        <tbody className='odd:bg-gray-200'>{renderTable()}</tbody>
+      </table>
+    </div>
   );
 }
