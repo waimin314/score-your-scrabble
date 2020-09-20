f54a45e274cada3867bb526f3f39797e8a6c1caa
Wai Min <leopard23@gmail.com>
2020-09-20 21:01:28 +0800

    Revert server url hardcoding

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = process.env.SERVER_URL || 'http://localhost:5000/';
-const BASE_URL = 'https://stormy-mesa-70307.herokuapp.com/';
+const BASE_URL = process.env.SERVER_URL || 'http://localhost:5000/';
+// const BASE_URL = 'https://stormy-mesa-70307.herokuapp.com/';
 const API = axios.create({ baseURL: BASE_URL });
 
 const getAllEntries = async () => {
