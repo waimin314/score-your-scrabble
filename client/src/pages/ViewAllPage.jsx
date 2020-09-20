c97b87ff01353dc1b6ebfb02ab50d0353cf11095
Wai Min <leopard23@gmail.com>
2020-09-20 15:20:30 +0800

    Fix calling aync funcion directly from UseEffect

--- client/src/pages/ViewAllPage.jsx
+++ client/src/pages/ViewAllPage.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { getAllEntries } from './../util/API';
 
 export default function ViewAll() {
   const [entries, setEntries] = useState([]);
-  useEffect(async () => {
-    setEntries(await getAllEntries());
+  useEffect(() => {
+    const fetchEntries = async () => {
+      setEntries(await getAllEntries());
+    };
+    fetchEntries();
   }, []);
   const renderTable = () => {
     return entries.map(({ word, score }, index) => {
@@ -18,16 +22,23 @@ export default function ViewAll() {
     });
   };
   return (
-    <div className='flex justify-center mt-5'>
-      <table className='table-fixed'>
-        <thead>
-          <tr className='bg-gradient-to-r from-teal-400 to-blue-300 text-gray-800'>
-            <th className='w-1/2 border border-gray-700 px-4 py-2'>Word</th>
-            <th className='w-1/3 border border-gray-700 px-4 py-2'>Score</th>
-          </tr>
-        </thead>
-        <tbody className='odd:bg-gray-200'>{renderTable()}</tbody>
-      </table>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex justify-center my-5 overflow-scroll h-auto'>
+        <table className='table-fixed'>
+          <thead>
+            <tr className='bg-gradient-to-r from-teal-400 to-blue-300 text-gray-800'>
+              <th className='w-1/2 border border-gray-700 px-4 py-2'>Word</th>
+              <th className='w-1/3 border border-gray-700 px-4 py-2'>Score</th>
+            </tr>
+          </thead>
+          <tbody className='odd:bg-gray-200'>{renderTable()}</tbody>
+        </table>
+      </div>
+      <Link to='/'>
+        <button className='w-20 h-8 rounded-md bg-pink-700 text-white'>
+          Back
+        </button>
+      </Link>
     </div>
   );
 }
