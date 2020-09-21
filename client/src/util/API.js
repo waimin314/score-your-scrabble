76977d91b31ff7b7cab49c5c3c82f5f3ad8a5c0d
Wai Min <leopard23@gmail.com>
2020-09-21 23:37:54 +0800

    Prefix REACT_APP_ for SERVER_URL

--- client/src/util/API.js
+++ client/src/util/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.SERVER_URL || 'http://localhost:5000/';
+const BASE_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000/';
 // const BASE_URL = 'https://stormy-mesa-70307.herokuapp/';
 const API = axios.create({ baseURL: BASE_URL });
 
