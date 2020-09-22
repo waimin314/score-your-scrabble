2515ef415956984dbcc2b6e7bdb9b7646ce093ff
Wai Min <leopard23@gmail.com>
2020-09-22 19:44:38 +0800

    Add test for sending invalid word

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,14 @@
 import { calculateScore } from './util/ScoreCalculator';
+import { saveEntry } from './util/API';
 
 it('Calculate score', () => {
   expect(calculateScore('EXCITING')).toEqual(18);
   expect(calculateScore('QUIZZIFY')).toEqual(41);
+});
+
+it('Send invalid word', async () => {
+  let entry = { letters: 'ASDFG', score: 111 };
+  let { alertType, message } = await saveEntry(entry);
+  expect(alertType).toEqual('Error');
+  expect(message).toEqual(`${entry.letters} is not a valid word`);
 });
